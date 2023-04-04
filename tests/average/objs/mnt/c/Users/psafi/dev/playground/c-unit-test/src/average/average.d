8abbfbe292a1c83e535832af5052af40e34b4468
Pedram Safi <psafi@swiftengineering.com>
2023-04-04 11:44:50 -0700

    Created Unit test framework

---
+++ tests/average/objs/mnt/c/Users/psafi/dev/playground/c-unit-test/src/average/average.d
@@ -0,0 +1,12 @@
+objs//mnt/c/Users/psafi/dev/playground/c-unit-test/src/average/average.o: \
+ /mnt/c/Users/psafi/dev/playground/c-unit-test/src/average/average.c \
+ /mnt/c/Users/psafi/dev/playground/c-unit-test/tools/cpputest/include/CppUTest/MemoryLeakDetectorMallocMacros.h \
+ /mnt/c/Users/psafi/dev/playground/c-unit-test/tools/cpputest/include/CppUTest/CppUTestConfig.h \
+ /mnt/c/Users/psafi/dev/playground/c-unit-test/tools/cpputest/include/CppUTest/CppUTestGeneratedConfig.h \
+ /mnt/c/Users/psafi/dev/playground/c-unit-test/tools/cpputest/include/CppUTest/StandardCLibrary.h \
+ /mnt/c/Users/psafi/dev/playground/c-unit-test/src/average/average.h
+/mnt/c/Users/psafi/dev/playground/c-unit-test/tools/cpputest/include/CppUTest/MemoryLeakDetectorMallocMacros.h:
+/mnt/c/Users/psafi/dev/playground/c-unit-test/tools/cpputest/include/CppUTest/CppUTestConfig.h:
+/mnt/c/Users/psafi/dev/playground/c-unit-test/tools/cpputest/include/CppUTest/CppUTestGeneratedConfig.h:
+/mnt/c/Users/psafi/dev/playground/c-unit-test/tools/cpputest/include/CppUTest/StandardCLibrary.h:
+/mnt/c/Users/psafi/dev/playground/c-unit-test/src/average/average.h:
